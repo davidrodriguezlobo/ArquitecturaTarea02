a84c2592a1da22f93daca3c259f1d74ed1975749
davidrodriguezlobo <35872962+davidrodriguezlobo@users.noreply.github.com>
2018-03-20 22:24:57 -0600

    Se finaliza el codigo base.
    Se finaliza la conversión de tipos de datos, (str si significa string) y se agrega unidades en mensaje impreso.

--- Codigo 3 - Calcular Corriente - Python/Corriente.py
+++ Codigo 3 - Calcular Corriente - Python/Corriente.py
@@ -1,6 +1,6 @@
 # File Corriente.py
 
-v = int(input("Ingresar el voltaje..."))
-r = int(input("Ingrese la resistencia..."))
+v = int(input("Ingresar el voltaje... "))
+r = int(input("Ingrese la resistencia... "))
 c = v/r
-print("La corriente es "+c)
+print("La corriente es " +str(c) +" Amperios.")
