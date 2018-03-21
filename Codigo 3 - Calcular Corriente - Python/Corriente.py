bf2d06e66498096260a6c2beb8a9914256af0519
davidrodriguezlobo <35872962+davidrodriguezlobo@users.noreply.github.com>
2018-03-20 22:20:04 -0600

    Elimino Directorio y Creo Codigo Base
    Elimino directorio creado accidentalmente y creo codigo base. Da error el tipo de dato. Nota: investigar que es tipo de dato str (string?).

--- Codigo 3 - Calcular Corriente - Python/Corriente.py
+++ Codigo 3 - Calcular Corriente - Python/Corriente.py
@@ -1,1 +1,6 @@
 # File Corriente.py
+
+v = int(input("Ingresar el voltaje..."))
+r = int(input("Ingrese la resistencia..."))
+c = v/r
+print("La corriente es "+c)
--- Codigo 3 - Calcular Resistencia - Python/Corriente.py
+++
@@ -1,6 +0,0 @@
-# File Corriente.py
-
-v = input('Ingresar el voltaje...')
-r = input('Ingrese la resistencia')
-c = v/r
-print(c)
