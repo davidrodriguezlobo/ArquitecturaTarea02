44808d538b6fe4bd5ae596770ac96dce3d93e5a4
davidrodriguezlobo <35872962+davidrodriguezlobo@users.noreply.github.com>
2018-03-20 21:33:36 -0600

    Explicacion Escrita del Calculo
    Se agrega por medio de comentario la explicación de los calculos.

--- Codigo 2 - Calcular Peso - PHP/index.php
+++ Codigo 2 - Calcular Peso - PHP/index.php
@@ -8,11 +8,11 @@ Ingrese la Fuerza Aplicada:
 <?php
     if(isset($_POST['submit']))
     {
-        $Fuerza = $_POST['Fuerza'];
-        $Gravedad = 9.8;
-        $Peso =  $Fuerza/$Gravedad;
+        $Fuerza = $_POST['Fuerza']; //La fuerza es cambiante.
+        $Gravedad = 9.8; //La Gravedad es fija.
+        $Peso =  $Fuerza/$Gravedad; //El peso se despeja a partir de Fuerza = Masa / Gravedad, donde la Gravedad pasa a dividir la Fuerza.
 echo "El Peso, cuando se aplica una Fuerza de $Fuerza Newtons y se mantiene una Gravedad de $Gravedad es: ".$Peso." Kg.";
-}
+	}
 ?>
 </body>
 </html>
