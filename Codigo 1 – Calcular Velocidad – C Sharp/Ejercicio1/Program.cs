2d005aa8f67335ad53b15ca97c45fa8479d07826
davidrodriguezlobo <35872962+davidrodriguezlobo@users.noreply.github.com>
2018-03-20 21:02:38 -0600

    Creacion Codigo
    Creacion Calculo de Velocidad (intentar division entre cero)

--- Codigo 1 – Calcular Velocidad – C Sharp/Ejercicio1/Program.cs
+++ Codigo 1 – Calcular Velocidad – C Sharp/Ejercicio1/Program.cs
@@ -10,7 +10,26 @@ namespace Ejercicio1
     {
         static void Main(string[] args)
         {
-            
+            double x = 0;
+            double y = 0;
+            double v = 0;
+
+            Console.WriteLine("Ingresar la distancia recorrida en kilometros...");
+            x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresar la el tiempo transcurrido...");
+            y = double.Parse(Console.ReadLine());
+
+            if (y == 0)
+            {
+                Console.WriteLine("Te has teletransportado!! No existe velocidad alguna!");
+                Console.ReadLine();
+            }
+            else
+            {
+                v = x / y;
+                Console.WriteLine("Tu velocidad es de: " + v +" Km/Hr");
+                Console.ReadLine();
+            }
         }
     }
 }
